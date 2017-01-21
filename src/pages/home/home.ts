5147efb5f89210038b469ffbdbb824f3f8728a59
Juan David Sánchez <juancho85@gmail.com>
2017-01-21 19:02:47 +0100

    lifecycle hooks

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {UsersPage} from "../users/users";
 import {ShopPage} from "../shop/shop";
 import {BuyoutPage} from "../buyout/buyout";
+import {NavController} from "ionic-angular";
 
 @Component({
   selector: 'page-home',
@@ -12,6 +13,13 @@ export class HomePage {
   usersPage = UsersPage;
   shopPage = ShopPage;
   buyoutPage = BuyoutPage;
+
+  constructor(private navCtrl: NavController) {}
+
+  onGoToUsers() {
+    this.navCtrl.push(this.usersPage)
+      .catch((error)=>console.log('Access denied', 'Argument was ' + error));
+  }
 
 
 }
--- src/pages/users/users.ts
+++ src/pages/users/users.ts
@@ -14,5 +14,41 @@ export class UsersPage {
     this.navCtrl.push(UserPage, {userName: name});
   }
 
+  ionViewCanEnter(): boolean | Promise<void> {
+    console.log('ionViewCanEnter');
+    const rnd = Math.random();
+    return rnd > 0.1;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad');
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+  }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+  ionViewCanLeave(): boolean | Promise<boolean> {
+    console.log('ionViewCanLeave');
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 1000);
+    });
+    return promise
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ionViewWillUnload() {
+    console.log('ionViewWillUnload');
+  }
+
+
 
 }
